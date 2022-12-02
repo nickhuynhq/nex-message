ae70eab86831ac5e767f1a6d28c4ba9b9b8f089e
Nicholas Huynh <nickhuynhq@gmail.com>
2022-12-02 15:16:18 -0500

    Set up NextAuth and Prisma Adapter to be used with MongoDB

---
+++ src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,18 @@
+import NextAuth from "next-auth"
+import GoogleProvider from "next-auth/providers/google"
+import { PrismaAdapter } from "@next-auth/prisma-adapter"
+import { PrismaClient } from "@prisma/client"
+
+const prisma = new PrismaClient();
+
+// Prisma is middle man between NEXT app and MongoDB database
+
+export default NextAuth({
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }),
+  ],
+})
