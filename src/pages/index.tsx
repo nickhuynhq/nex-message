7b6b3c60135c99146253de1b7ff639bdb2e62127
Nicholas Huynh <nickhuynhq@gmail.com>
2022-12-02 16:45:21 -0500

    Add custom property to next-auth User interface

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,7 @@ import { NextPage, NextPageContext } from "next";
 import { getSession, useSession } from "next-auth/react";
 import Auth from "../components/Auth/Auth";
 import Chat from "../components/Chat/Chat";
+import { Session } from "next-auth";
 
 const Home: NextPage = () => {
   const { data } = useSession();
@@ -10,7 +11,7 @@ const Home: NextPage = () => {
 
   return (
     <Box>
-      {data?.user ? <Chat /> : <Auth />}
+      {data?.user?.username ? <Chat /> : <Auth />}
     </Box>
   )
 }
