1bfc88fda287e3d75d701088b4dd556b6efb806a
Nicholas Huynh <nickhuynhq@gmail.com>
2022-12-02 16:32:23 -0500

    Set up Chat and Auth components

---
+++ src/components/Auth/Auth.tsx
@@ -0,0 +1,13 @@
+
+interface IAuthProps {
+}
+
+const Auth: React.FC<IAuthProps> = (props) => {
+  return (
+    <div>
+        AUTH
+    </div>
+  );
+};
+
+export default Auth;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,18 @@
+import { Box } from "@chakra-ui/react";
 import { NextPage, NextPageContext } from "next";
-import { getSession, signIn, signOut, useSession } from "next-auth/react";
+import { getSession, useSession } from "next-auth/react";
+import Auth from "../components/Auth/Auth";
+import Chat from "../components/Chat/Chat";
 
 const Home: NextPage = () => {
   const { data } = useSession();
   console.log(data);
 
   return (
-    <div>
-      {data?.user ? (
-        <button onClick={() => signOut()}>Sign Out</button>
-      ) : (
-        <button onClick={() => signIn("google")}>Sign In</button>
-      )}
-
-      {data?.user?.name}
-    </div>
-  );
+    <Box>
+      {data?.user ? <Chat /> : <Auth />}
+    </Box>
+  )
 }
 
 export async function getServerSideProps(context: NextPageContext) {
