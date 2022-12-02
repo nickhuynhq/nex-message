f12d58886215316bd7268a64fd36fa4a684b7f00
Nicholas Huynh <nickhuynhq@gmail.com>
2022-12-02 16:06:12 -0500

    Set up Chakra UI and theme

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,15 @@
 
-import { SessionProvider } from 'next-auth/react'
-import type { AppProps } from 'next/app'
+import { ChakraProvider } from '@chakra-ui/react';
+import { SessionProvider } from 'next-auth/react';
+import {theme} from "../chakra/theme";
+import type { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps: {session, ...pageProps}}: AppProps) {
   return (
     <SessionProvider session={session}>
-      <Component {...pageProps} />
+      <ChakraProvider theme={theme}>
+        <Component {...pageProps} />
+      </ChakraProvider>
     </SessionProvider>
   );
 }
