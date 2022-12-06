96fae746368923ae8208e64f3d6e9073cf34fb30
Nicholas Huynh <nickhuynhq@gmail.com>
2022-12-06 13:38:13 -0500

    Set up links to use apollo web sockets

--- src/graphql/apollo-client.ts
+++ src/graphql/apollo-client.ts
@@ -1,11 +1,43 @@
-import { ApolloClient, HttpLink, InMemoryCache, ApolloProvider, gql } from '@apollo/client';
+import {
+  ApolloClient,
+  HttpLink,
+  InMemoryCache,
+  split,
+} from "@apollo/client";
+import { GraphQLWsLink } from "@apollo/client/link/subscriptions";
+import { getMainDefinition } from "@apollo/client/utilities";
+import { createClient } from "graphql-ws";
+import { type } from "os";
 
 const httpLink = new HttpLink({
-    uri: "http://localhost:4000/graphql",
-    credentials: "include",
+  uri: "http://localhost:4000/graphql",
+  credentials: "include",
 });
 
+// Check we are in web browser and not in Next.js server
+// This is due to Next SSR
+const wsLink =
+  typeof window !== "undefined"
+    ? new GraphQLWsLink(
+        createClient({
+          url: "ws://localhost:4000/graphql/subscriptions",
+        })
+      )
+    : null;
+
+const link = typeof window !== 'undefined' && wsLink != null ? split(
+    ({ query }) => {
+      const definition = getMainDefinition(query);
+      return (
+        definition.kind === 'OperationDefinition' &&
+        definition.operation === 'subscription'
+      );
+    },
+    wsLink,
+    httpLink,
+  ) : httpLink;
+
 export const client = new ApolloClient({
-    link: httpLink,
-    cache: new InMemoryCache(),
+  link,
+  cache: new InMemoryCache(),
 });
