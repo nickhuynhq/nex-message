17909a35a04a759f24182b766b21f2049e768e9e
Nicholas Huynh <nickhuynhq@gmail.com>
2022-12-12 11:44:02 -0500

    Create and format the Message Input component

--- src/components/Chat/Feed/FeedWrapper.tsx
+++ src/components/Chat/Feed/FeedWrapper.tsx
@@ -2,6 +2,7 @@ import { Flex } from "@chakra-ui/react";
 import { Session } from "next-auth";
 import { useRouter } from "next/router";
 import MessagesHeader from "./Messages/Header";
+import MessageInput from "./Messages/Input";
 
 interface FeedWrapperProps {
   session: Session;
@@ -22,15 +23,18 @@ const FeedWrapper: React.FC<FeedWrapperProps> = ({ session }) => {
       direction="column"
     >
       {conversationId && typeof conversationId === "string" ? (
-        <Flex
-          direction="column"
-          justify="space-between"
-          overflow="hidden"
-          flexGrow={1}
-        >
-          {/* {conversationId} */}
-          <MessagesHeader userId={userId} conversationId={conversationId} />
-        </Flex>
+        <>
+          <Flex
+            direction="column"
+            justify="space-between"
+            overflow="hidden"
+            flexGrow={1}
+          >
+            {/* {conversationId} */}
+            <MessagesHeader userId={userId} conversationId={conversationId} />
+          </Flex>
+          <MessageInput  session={session} conversationId={conversationId} />
+        </>
       ) : (
         <div>No Conversation Selected</div>
       )}
---
+++ src/components/Chat/Feed/Messages/Input.tsx
@@ -0,0 +1,45 @@
+import { Box, Input } from "@chakra-ui/react";
+import { Session } from "next-auth";
+import React, { useState } from "react";
+import { toast } from "react-hot-toast";
+
+type MessageInputProps = {
+  session: Session;
+  conversationId: string;
+};
+
+const MessageInput = ({ session, conversationId }: MessageInputProps) => {
+  const [messageBody, setMessageBody] = useState("");
+
+  const onSendMessage = async (event: React.FormEvent) => {
+    event.preventDefault();
+
+    try {
+      
+    } catch (error: any) {
+      console.log("onSendMessage error", error)
+      toast.error(error?.message)
+    }
+  }
+
+  return (
+    <Box px={4} py={6} width="100%">
+      <form onSubmit={() => {}}>
+        <Input
+          value={messageBody}
+          size="md"
+          placeholder="New Message"
+          onChange={(event) => setMessageBody(event.target.value)}
+          resize="none"
+          _focus={{
+            boxShadow: "none",
+            border: "1px solid",
+            borderColor: "whiteAlpha.300",
+          }}
+        />
+      </form>
+    </Box>
+  );
+};
+
+export default MessageInput;
