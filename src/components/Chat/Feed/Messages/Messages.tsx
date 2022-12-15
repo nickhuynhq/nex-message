03062533ea7635cc4da1361f753e35c2602dc265
Nicholas Huynh <nickhuynhq@gmail.com>
2022-12-14 19:53:08 -0500

    Invoke subscribeToMoreMessages subscription in Messages feed

--- src/components/Chat/Feed/Messages/Messages.tsx
+++ src/components/Chat/Feed/Messages/Messages.tsx
@@ -1,51 +1,77 @@
 import { useQuery } from "@apollo/client";
 import { Flex, Stack } from "@chakra-ui/react";
+import { useEffect } from "react";
 import { toast } from "react-hot-toast";
 import MessageOperations from "../../../../graphql/operations/message";
-import { MessagesData, MessagesVariables } from "../../../../util/types";
+import {
+  MessagesData,
+  MessagesVariables,
+  MessageSubscriptionData,
+} from "../../../../util/types";
 import SkeletonLoader from "../../../common/SkeletonLoader";
-
 interface MessagesProps {
   userId: string;
   conversationId: string;
 }
 
 const Messages: React.FC<MessagesProps> = ({ userId, conversationId }) => {
-  const { data, loading, error, subscribeToMore } = useQuery<MessagesData, MessagesVariables>(
-    MessageOperations.Query.messages,
-    {
+  const { data, loading, error, subscribeToMore } = useQuery<
+    MessagesData,
+    MessagesVariables
+  >(MessageOperations.Query.messages, {
+    variables: {
+      conversationId,
+    },
+    onError: ({ message }) => {
+      toast.error(message);
+    },
+  });
+
+  const subscribeToMoreMessages = (conversationId: string) => {
+    subscribeToMore({
+      document: MessageOperations.Subscription.messageSent,
       variables: {
         conversationId,
       },
-      onError: ({message}) => {
-        toast.error(message);
+      updateQuery: (prev, { subscriptionData }: MessageSubscriptionData) => {
+        if (!subscriptionData) return prev;
+
+        console.log("HERE IS SUBSCRIPTION DATA", subscriptionData)
+
+        const newMessage = subscriptionData.data.messageSent;
+        return Object.assign({}, prev, {
+          messages: [newMessage, ...prev.messages],
+        });
       },
-    }
-  );
+    });
+  };
+
+  useEffect(() => {
+    subscribeToMoreMessages(conversationId)
+  },[conversationId])
 
   if (error) {
     return null;
   }
 
-  console.log("HERE IS MESSAGES DATA", data);
-  
-
-  return <Flex direction="column" justify="flex-end" overflow="hidden">
-    {loading && (
+  return (
+    <Flex direction="column" justify="flex-end" overflow="hidden">
+      {loading && (
         <Stack px={4} spacing={4}>
-            <SkeletonLoader count={4} height="60px" width="100%"/>
+          <SkeletonLoader count={4} height="60px" width="100%" />
         </Stack>
-    )}
+      )}
 
-    {data?.messages && (
+      {data?.messages && (
         <Flex direction="column-reverse" overflowY="scroll" height="100%">
-            {data.messages.map((message) => (
-                // <MessageItem />
-                <div key={message.id}>{message.body}</div>
-            ))}
+          {data.messages.map((message) => (
+            // <MessageItem />
+            <div key={message.id}>{message.body}</div>
+          ))}
         </Flex>
-    )}
-  </Flex>;
+      )}
+    </Flex>
+  );
 };
 
 export default Messages;
--- src/util/types.ts
+++ src/util/types.ts
@@ -51,3 +51,11 @@ export interface MessagesData {
 export interface MessagesVariables {
   conversationId: string;
 }
+
+export interface MessageSubscriptionData {
+  subscriptionData: {
+    data: {
+      messageSent: MessagePopulated;
+    }
+  }
+}
