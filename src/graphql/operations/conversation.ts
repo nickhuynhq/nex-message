fe3594ab9b22f37cc17cee004952089856315123
Nicholas Huynh <nickhuynhq@gmail.com>
2022-12-16 13:03:21 -0500

    Add markConversationAsRead mutation in operations

--- src/graphql/operations/conversation.ts
+++ src/graphql/operations/conversation.ts
@@ -35,6 +35,14 @@ const ConversationOperations = {
         }
       }
     `,
+    markConversationAsRead: gql`
+      mutation MarkConversationAsRead(
+        $userId: String!
+        $conversationId: String!
+      ) {
+        markConversationAsRead(userId: $userId, conversationId: $conversationId)
+      }
+    `,
   },
 
   Subscriptions: {
