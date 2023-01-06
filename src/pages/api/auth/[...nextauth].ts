50f071b4aa501c2ba851ae59dc45e63096dd5639
Nicholas Huynh <nickhuynhq@gmail.com>
2023-01-06 15:44:55 -0500

    Console log for testing

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -17,6 +17,7 @@ export default NextAuth({
   secret: process.env.NEXTAUTH_SECRET,
   callbacks: {
     async session({ session, token, user }) {
+      console.log(session)
       return {...session, user: {...session.user, ...user}}
     }
   }
