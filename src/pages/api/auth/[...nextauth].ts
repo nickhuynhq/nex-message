944c39875a0aba20c565aa98d57da979634189eb
Nicholas Huynh <nickhuynhq@gmail.com>
2023-01-14 11:14:31 -0500

    Set useSecureCookies to false in authoptions

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -15,6 +15,7 @@ export const authOptions: NextAuthOptions = {
     }),
   ],
   secret: process.env.NEXTAUTH_SECRET,
+  useSecureCookies: false,
   callbacks: {
     async session({ session, token, user }) {
       console.log(session);
